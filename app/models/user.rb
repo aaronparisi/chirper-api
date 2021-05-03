29268358ab082324021ef7ba2a76233a78875304
Aaron <parisi.aaron@gmail.com>
2021-05-03 23:16:17 +0000

    create user model, basic field validations

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password_digest, :session_token, :email, presence: true
+  validate :valid_email
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end  
+
+  def valid_email
+    if ! self.email =~ /^(.+)@(.+)$/
+      errors.add(:email, "must be a valid e-mail address")
+    end
+  end
+
+  def password=(password)
+    @password = password
+
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+  
+  def ensure_session_token
+    # Generate the initial session_token so that we pass the validation
+    # This method runs right after the model is initialized, before any validations are run
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    # When a user logs out, we want to scramble their session_token so that bad people cannot use the old one
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+end
